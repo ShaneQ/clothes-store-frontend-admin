2ee62f374aa9bf61026511e1d6cf65336055fa9d
shane <shanequaid@hotmail.com>
2020-06-03 17:00:25 +0100

    Removed unneeded code

--- src/main/resources/src/app/home/home.component.ts
+++ src/main/resources/src/app/home/home.component.ts
@@ -1,4 +1,4 @@
-import {Component} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {AppService} from '../app.service'
 
 @Component({
@@ -9,24 +9,14 @@ import {AppService} from '../app.service'
 
 })
 
-export class HomeComponent {
+export class HomeComponent implements OnInit{
   public isLoggedIn = false;
 
   constructor(
     private _service: AppService){}
 
-  // tslint:disable-next-line:use-lifecycle-interface
   ngOnInit(){
     this.isLoggedIn = this._service.checkCredentials();
-    const i = window.location.href.indexOf('code');
-    if (!this.isLoggedIn && i !== -1){
-      this._service.retrieveToken(window.location.href.substring(i + 5));
-    }
-  }
-
-  login() {
-    window.location.href = 'http://localhost:8083/auth/realms/baeldung/protocol/openid-connect/auth?response_type=code&&scope=write%20read&client_id=' +
-      this._service.clientId + '&redirect_uri=' + this._service.redirectUri;
   }
 
   logout() {
