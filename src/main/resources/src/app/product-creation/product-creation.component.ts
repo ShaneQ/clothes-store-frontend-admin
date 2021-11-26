eab73aa317ca1cde58f6e84eeda2919b4b179771
shanequaid-toast <shane.quaid@toasttab.com>
2021-11-26 18:23:55 +0000

    Added product creation form

--- src/main/resources/src/app/product-creation/product-creation.component.ts
+++ src/main/resources/src/app/product-creation/product-creation.component.ts
@@ -1,8 +1,7 @@
 import {Component, OnInit} from '@angular/core';
-import {FormArray, FormBuilder, FormControl, FormGroup} from "@angular/forms";
+import {FormArray, FormBuilder, FormControl, FormGroup, Validators} from "@angular/forms";
 import {Product} from "../model/product";
 import {ProductService} from "../product.service";
-import {ScriptService} from "../script.service";
 
 @Component({
   selector: 'app-product-creation',
@@ -13,45 +12,107 @@ import {ScriptService} from "../script.service";
 export class ProductCreationComponent implements OnInit {
   productForm: FormGroup
 
-  Sizes: Array<any> = [
-    { name: 'S', value: '2' },
-    { name: 'M', value: '3' },
-    { name: 'L', value: '4' }
+  sizes: Array<any> = [
+    {name: 'One Size', value: '1'},
+    {name: 'XS', value: '2'},
+    {name: 'S', value: '3'},
+    {name: 'M', value: '4'},
+    {name: 'L', value: '5'},
+    {name: 'XL', value: '6'}
+
   ];
+
+  productCategories = []
+  colors = []
+  seasons = []
   imageUrl: string;
 
   constructor(private fb: FormBuilder, private _app: ProductService,) {
   }
 
   ngOnInit(): void {
-    this.initilizeForm();
+    this.initializeForm();
   }
 
   submit() {
     let product: Product;
     product = this.productForm.value;
     product.dryClean = this.productForm.get("dryClean").value
-    console.log(product)
     this._app.postProduct(product)
   }
 
-  private initilizeForm() {
+  private initializeForm() {
 
     this.productForm = this.fb.group({
-      name: 'Testing',
-      quickDesc: 'ewrweferg',
-      fittingInfo: 'asdffd',
-      washInfo: 'asfdsdf',
-      material: 'sfsf',
+      name: ['Testing', [Validators.required]],
+      quickDesc: ['Testing', [Validators.required]],
+      description: ['Testing', [Validators.required]],
+      fittingInfo: ['Testing', [Validators.required]],
+      washInfo: ['Testing', [Validators.required]],
+      material: ['Testing', [Validators.required]],
       imgCover: this.fb.group({
-          url: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT1kUVuAjGZ8a6N1Tz9m0i0zKXVkk0CTJnJslJu6Z3Pk17XqOdSyQ&s',
-          id:0
-        }),
+        url: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT1kUVuAjGZ8a6N1Tz9m0i0zKXVkk0CTJnJslJu6Z3Pk17XqOdSyQ&s',
+        id: 0
+      }),
       dryClean: false,
+      productCategory: [1],
+      color: [1],
+      season: [1],
+      retailPrice: [0, [Validators.required]],
+      measurements: this.fb.group({
+        chest: [0, [Validators.required]],
+        hips: [0, [Validators.required]],
+        waist: [0, [Validators.required]],
+        length: [0, [Validators.required]]
+      }),
       sizes: this.fb.array([]),
       images: this.fb.array([])
     })
+    this.productCategories = this.getProductCategories()
+    this.colors = this.getColors()
+    this.seasons = this.getSeasons()
   }
+
+  getProductCategories() {
+    return [
+      {name: 'Dresses', id: '1'},
+      {name: 'Tops', id: '2'},
+      {name: 'Pants', id: '3'},
+      {name: 'Skirts', id: '4'},
+      {name: 'Jumpsuits & Rompers', id: '5'},
+      {name: 'Jackets & Coats', id: '6'},
+      {name: 'Bags', id: '7'},
+    ];
+  }
+  getSeasons() {
+    return [
+      {name: 'Winter', id: '1'},
+      {name: 'Spring', id: '2'},
+      {name: 'Fall', id: '3'},
+      {name: 'Summer', id: '4'}
+    ];
+  }
+  getColors() {
+    return [
+      {name: 'black', id: 1},
+      {name: 'white', id: 2},
+      {name: 'grey', id: 3},
+      {name: 'cream', id: 4},
+      {name: 'brown', id: 5},
+      {name: 'red', id: 6},
+      {name: 'orange', id: 7},
+      {name: 'yellow', id: 8},
+      {name: 'green', id: 9},
+      {name: 'blue', id: 10},
+      {name: 'purple', id: 11},
+      {name: 'pink', id: 12},
+      {name: 'gold', id: 13},
+      {name: 'silver', id: 14},
+      {name: 'print', id: 15},
+
+    ];
+  }
+
 
   onCheckboxChange(e) {
     const checkArray: FormArray = this.productForm.get('sizes') as FormArray;
@@ -75,7 +136,8 @@ export class ProductCreationComponent implements OnInit {
       url: 'https://media.geeksforgeeks.org/wp-content/uploads/20190506164011/logo3.png'
     })
   }
-  get images(){
+
+  get images() {
     return this.productForm.get('images') as FormArray
   }
 
@@ -84,6 +146,6 @@ export class ProductCreationComponent implements OnInit {
   }
 
   removeImage(i: number) {
-      this.images.removeAt(i)
+    this.images.removeAt(i)
   }
 }
